b646d1ab5214e16e301a640211ddb4128da39296
DiegzM <147036619+DiegzM@users.noreply.github.com>
2025-09-02 15:26:43 -0700

    First commit

---
+++ Quicksort/src/main/java/Quicksort.java
@@ -0,0 +1,8 @@
+public class Quicksort {
+    public int[] quickSortRecursive(int[] arr) {
+        int pivot = arr[0];
+    }
+    public static void main(String[] args) {
+        int[] arr = [5, 3, 0, 100, 23, 49, 24];
+    }
+}
